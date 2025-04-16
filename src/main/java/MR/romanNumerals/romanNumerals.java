d7154cc4a79d75293d4f9cb05dbe6a9bbcc6ae02
MaxR112 <maxr9090@gmail.com>
2025-04-16 17:35:15 +0000

    code: testConvert978

--- src/main/java/MR/romanNumerals/romanNumerals.java
+++ src/main/java/MR/romanNumerals/romanNumerals.java
@@ -3,8 +3,8 @@ package MR.romanNumerals;
 public class romanNumerals {
     public String convert(int numericValue){
         String romanValue = "";
-        int[] numericList = {1000, 500, 400, 100, 90, 10, 9, 5, 4, 1};
-        String[] romanList = {"M", "D", "CD", "C", "XC", "X", "IX", "V", "IV", "I"};
+        int[] numericList = {1000, 900, 500, 400, 100, 90, 50, 10, 9, 5, 4, 1};
+        String[] romanList = {"M", "CM", "D", "CD", "C", "XC", "L", "X", "IX", "V", "IV", "I"};
 
         if (numericValue < 0){
             throw new NegativeIntegerException();
